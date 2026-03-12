fdbd4bfdc2d4d9971b7a98ddd25a289066f5a827
amauri.junior <mendesamauri177@gmail.com>
2026-03-12 10:31:19 -0300

    Projeto Prático: Criando sistema de gerenciamento de petshop

---
+++ 02-Pratica  js/petshop.js
@@ -0,0 +1,26 @@
+let pet = {
+    nome:'Bidu' ,
+    idade: 12 ,
+    vacinado: true ,
+    servicos: ['Banho', 'Tosa', 'Massagem'],
+}
+// Para chamar o texto, utilizamos o 'entre textos' e para acessar o objeto e seu valor, 
+// utilizamos o nome do indice do objeto com . e o valor pet.nome (pet é o nome do objeto, e o nome é o que eu quero saber)
+// Para acessar o indice correto do objeto, utilizamos entre [] o número do 'serviço' entre chaves. ' pet.serviços[1] ' 
+// resultado // console.log('Nome do peté :', pet.nome, 'e o serviço escolhido é', pet.servicos[1] ); 
+
+let saldo = 100;
+let jogo = 30;
+const bonus = 20;
+// bonus = 10;
+// resultado // console.log('João comprou 1 jogo de 30$ e tem 100$. João ficou com',(saldo - jogo));
+
+
+// let compras = ['Arroz', 'Feijão', 'Ovo'];
+// let index = compras.indexOf('Feijão');
+// if (index !== -1) {
+//    compras.splice(index, 1, 'Macarrão')
+//}
+//console.log(compras)
+
+
