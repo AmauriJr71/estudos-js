329a403aa167055341bbdfc9fd6c5df5f4483942
amauri.junior <mendesamauri177@gmail.com>
2026-03-12 10:29:18 -0300

    Explicação: Aprendendo a declarar variáveis com let, const e var

---
+++ 01-conceitos-js/01-variaveis.js
@@ -0,0 +1,31 @@
+// Pode ser alterado!
+var nome = 'Amauri' // string - texto
+nome = 'Pedro'
+console.log('nome')
+
+// // Pode ser alterado!
+let idade = 20 // numeros 12 12.5 
+idade = 30
+
+// NÃO pode ser alterado!
+const cidade = 'Tatui'
+// Não foi alterado porque é um uma constante // cidade = 'Boituva'
+
+var maior_idade = true // boolean - verdadeiro ou falso
+
+const paises = ['Brasil', 'Argentina', 'Alemanha']; // array é uma Lista de dados
+
+// Objeto - Chave - Valor
+let devices = {
+    nome: 'Iphone 14',
+    preco: 8000,
+    cores: {
+        cor1: 'vermelho',
+        cor2: 'azul'
+    }
+} 
+
+
+
+
+
