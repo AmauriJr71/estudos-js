39da921b5cf5a0c2f6698300073ac80f90f0556c
amauri.junior <mendesamauri177@gmail.com>
2026-03-12 10:29:27 -0300

    Explicação: Manipulação de listas e métodos de array no JS

---
+++ 01-conceitos-js/02-array.js
@@ -0,0 +1,38 @@
+// Array é uma lsita com dados 
+// O array, ele é feito de indices que se iniciam no 0, o primeiro da lista no caso
+//Para acessar, [numero do indice]
+
+const paises = ['Brasil', 'Argentina', 'Alemanha']
+
+console.log('Acessando o país : ' + paises[0]) // Para concatenar, temos a possibilidade do ' + ' ou ' , ' 
+
+console.log(' Tamanho do meu array: ', paises.length) // tamanho do array = 3 brasil arg e almanha
+
+
+// Operações com array
+
+paises.push('USA') // Adicionando um elemento no FINAL do meu array
+paises.pop() // Remove o ultimo elemento da nossa array 
+paises.unshift('França') // Adiciona um elemento na primeira possição da array ' 0 '
+paises.shift() // Remove o primeiro elemento da nossa array
+
+console.log(paises)
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+// testando github
